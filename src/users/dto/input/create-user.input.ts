492db5775d7069caa7a98e051fdd354de8e84aea
Илья Маковецкий <illia.makovetsky@gmail.com>
2022-12-20 20:37:34 +0000

    finish

---
+++ src/users/dto/input/create-user.input.ts
@@ -0,0 +1,14 @@
+import { Field, InputType } from '@nestjs/graphql';
+import { IsNotEmpty, IsEmail } from 'class-validator';
+
+@InputType()
+export class CreateUserInput {
+  @Field()
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @Field()
+  @IsNotEmpty()
+  age: number;
+}
